5b49311587b28458d1657e7e97f57a5943b945f0
Sagar <sagar.patel@skyttus.com>
2024-06-10 12:37:21 +0530

    API change

--- model/PageContent.js
+++ model/PageContent.js
@@ -10,6 +10,10 @@ const PageContentSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  title2: {
+    type: String,
+    required: false,
+  },
   description: {
     type: String,
     required: true,
