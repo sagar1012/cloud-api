79bc215bff52fd8700fa44665b537ce1884d758d
Sagar <sagar.patel@skyttus.com>
2024-06-20 16:28:07 +0530

    Name add in schema

--- model/jobApply.js
+++ model/jobApply.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const jobApplySchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    code: { type: String, required: true, unique: true },
-    phoneNumber: { type: String, required: true },
-    email: { type: String, required: true },
+    title: { type: String },
+    code: { type: String, unique: true },
+    fullName: { type: String },
+    phoneNumber: { type: String },
+    email: { type: String },
     resume: { type: String }
 });
 
