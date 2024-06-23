02f1b0a8a13d92bd6d0cb5bec0695da508fff4b6
Sagar <patelritik42@gmail.com>
2024-06-23 12:35:55 +0530

    statust change login

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.post('/login', async (req, res) => {
         // Verify the password
         const passwordMatch = await bcrypt.compare(password, user.password);
         if (!passwordMatch) {
-            return res.status(401).json({ message: 'Invalid email or password' });
+            return res.status(200).json({ message: 'Invalid email or password' });
         }
 
         // Generate JWT token
