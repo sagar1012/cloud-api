96e9bc28803d2c43a5d230b18a9923c6fa95b7b2
Sagar <sagarkpatel179@gmail.com>
2024-07-22 19:43:41 +0530

    Change in schema

--- model/weather.js
+++ model/weather.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const weatherSchema = mongoose.Schema({
-    latitude: { type: Number },
-    longitude: { type: Number },
-    altitude: { type: Number },
-    maxTemperature: { type: Number },
-    minTemperature: { type: Number },
-    relHumidityMorning: { type: Number },
-    relHumidityAfternoon: { type: Number },
-    windSpeed: { type: Number },
-    brightSunshineHours: { type: Number },
-    evaporation: { type: Number },
-    rainFall: { type: Number },
-    rainyDays: { type: Number },
-    cumulativeRain: { type: Number },
+    latitude: { type: String },
+    longitude: { type: String },
+    altitude: { type: String },
+    maxTemperature: { type: String },
+    minTemperature: { type: String },
+    relHumidityMorning: { type: String },
+    relHumidityAfternoon: { type: String },
+    windSpeed: { type: String },
+    brightSunshineHours: { type: String },
+    evaporation: { type: String },
+    rainFall: { type: String },
+    rainyDays: { type: String },
+    cumulativeRain: { type: String },
 });
 
 module.exports = mongoose.model('weather', weatherSchema);
